428e853deccef94b03479829730f087fd94294d5
David Wandratsch <david.wandratsch@htl-wels.at>
2021-04-22 11:32:48 +0200

    initial commit

---
+++ main.c
@@ -0,0 +1,24 @@
+//*********************************************************************
+//
+//                H T L - W E L S   /   Klasse 3BHIT
+//
+//---------------------------------------------------------------------
+// Programm         : Buchverwaltung
+//
+// Ersteller        : David Wandratsch
+//
+// Erstelldatum     : 2021-04-22
+//
+// Kurzbeschreibung : Verwaltung und Abfrage von Büchern
+//
+// GitHub Repository: https://github.com/WandratschDavid/bookManagement
+//*********************************************************************
+
+// Including standard libraries
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World");
+    return 0;
+}
